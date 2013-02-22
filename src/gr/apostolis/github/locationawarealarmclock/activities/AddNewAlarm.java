348d7504381dd30d6f516f85e03b8af69b51a82a
Apostolis Github <a.github@gmail.com>
2013-02-22 23:17:26 +0200

    AddNewAlarm activity is functional
     - Basic layout for hdpi screens
     - Can add alarms (recurring or not)
     - Can select days when recurring should occur
     - No option to select effective location just yet

--- src/gr/apostolis/github/locationawarealarmclock/activities/AddNewAlarm.java
+++ src/gr/apostolis/github/locationawarealarmclock/activities/AddNewAlarm.java
@@ -1,16 +1,38 @@
 package gr.apostolis.github.locationawarealarmclock.activities;
 
-import gr.apostolis.github.locationawarealarmclock.R;
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import java.util.Arrays;
 
-public class AddNewAlarm extends Activity {
+import gr.apostolis.github.locationawarealarmclock.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.TimePicker;
+import android.widget.ToggleButton;
+
+public class AddNewAlarm extends Activity implements OnClickListener, OnCheckedChangeListener {
+
+	private static int[] daysIDList = new int[] { R.id.sun_tgl, R.id.mon_tgl,
+			R.id.tue_tgl, R.id.wed_tgl, R.id.thu_tgl, R.id.fri_tgl,
+			R.id.sat_tgl };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_new_alarm);
+		Button add = (Button) findViewById(R.id.add_button);
+		Button cancel = (Button) findViewById(R.id.cancel_button);
+		CheckBox repeat = (CheckBox) findViewById(R.id.repeat_chk);
+		repeat.setOnCheckedChangeListener(this);
+		add.setOnClickListener(this);
+		cancel.setOnClickListener(this);
 	}
 
 	@Override
@@ -18,6 +40,66 @@ public class AddNewAlarm extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_add_new_alarm, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		Intent intent = getIntent();
+		switch (v.getId()) {
+		case R.id.add_button:
+			intent.putExtra("time", getTime());
+			intent.putExtra("repeatOn", getRepeatOn());
+			setResult(RESULT_OK, intent);
+			break;
+		case R.id.cancel_button:
+		default:
+			setResult(RESULT_CANCELED, intent);
+			break;
+		}
+		finish();
+	}
+
+	public String getTime() {
+		TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker);
+		StringBuilder sb = new StringBuilder();
+		if (timePicker.getCurrentHour() < 10) {
+			sb.append("0");
+		}
+		sb.append(timePicker.getCurrentHour());
+		sb.append(":");
+		if (timePicker.getCurrentMinute() < 10) {
+			sb.append("0");
+		}
+		sb.append(timePicker.getCurrentMinute());
+		Log.d(getClass().toString(), sb.toString());
+		return sb.toString();
+	}
+
+	public boolean[] getRepeatOn() {
+		boolean[] repeatOn = null;
+		CheckBox repeat = (CheckBox) findViewById(R.id.repeat_chk);
+		if (repeat.isChecked()) {
+			repeatOn = new boolean[7];
+			ToggleButton toggle;
+			for (int i = 0; i < daysIDList.length; i++) {
+				toggle = (ToggleButton) findViewById(daysIDList[i]);
+				repeatOn[i] = toggle.isChecked();
+			}
+		}
+
+		Log.d(getClass().toString(), Arrays.toString(repeatOn));
+
+		return repeatOn;
+	}
+
+	@Override
+	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		ToggleButton toggle;
+		for (int i = 0; i < daysIDList.length; i++) {
+			toggle = (ToggleButton) findViewById(daysIDList[i]);
+			toggle.setEnabled(isChecked);
+		}
+
 	}
 
 }
