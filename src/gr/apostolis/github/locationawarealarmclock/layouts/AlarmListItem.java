1b4e15b36f6a6bb2264b0f3ab57bb1421f1dccd8
Apostolis Github <a.github@gmail.com>
2013-02-24 18:30:45 +0200

    Fixed animation inconsistencies
     - Items get deleted from the list only after the animation finishes
        to avoid having the wrong item get animated out
     - In order to do that, the Class triggering the animation event, also
        gets notified when the animation finishes, instead of having the
        list item being notified
     - Removed animation when inserting new alarms due to it's inconsistent
        nature (I implemented it all wrong)

--- src/gr/apostolis/github/locationawarealarmclock/layouts/AlarmListItem.java
+++ src/gr/apostolis/github/locationawarealarmclock/layouts/AlarmListItem.java
@@ -4,20 +4,15 @@ import gr.apostolis.github.locationawarealarmclock.R;
 import gr.apostolis.github.locationawarealarmclock.alarms.Alarm;
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.Animation.AnimationListener;
-import android.view.animation.AnimationUtils;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-public class AlarmListItem extends RelativeLayout implements AnimationListener {
+public class AlarmListItem extends RelativeLayout {
 
 	private ToggleButton toggleButton;
 	private TextView repeatText;
 	private TextView alarmTime;
-	private Animation anim = null;
 
 	public AlarmListItem(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -26,12 +21,6 @@ public class AlarmListItem extends RelativeLayout implements AnimationListener {
 	@Override
 	protected void onFinishInflate() {
 		super.onFinishInflate();
-		anim = AnimationUtils.makeInAnimation(getContext(), true);
-		anim.setFillAfter(true);
-		anim.setAnimationListener(this);
-		setVisibility(View.VISIBLE);
-		startAnimation(anim);
-
 		findViews();
 	}
 
@@ -46,25 +35,4 @@ public class AlarmListItem extends RelativeLayout implements AnimationListener {
 		repeatText.setText(alarm.getRepeatString());
 		toggleButton.setChecked(alarm.isActive());
 	}
-
-	public void animateOut(boolean toRight) {
-
-		anim = AnimationUtils.makeOutAnimation(getContext(), toRight);
-		anim.setAnimationListener(this);
-		startAnimation(anim);
-	}
-
-	@Override
-	public void onAnimationEnd(Animation arg0) {
-		anim = null;
-	}
-
-	@Override
-	public void onAnimationRepeat(Animation arg0) {
-	}
-
-	@Override
-	public void onAnimationStart(Animation arg0) {
-	}
-
 }
--- src/gr/apostolis/github/locationawarealarmclock/layouts/AlarmListItemOnGestureListener.java
+++ src/gr/apostolis/github/locationawarealarmclock/layouts/AlarmListItemOnGestureListener.java
@@ -2,16 +2,21 @@ package gr.apostolis.github.locationawarealarmclock.layouts;
 
 import gr.apostolis.github.locationawarealarmclock.adapters.AlarmListAdapter;
 import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
 import android.view.MotionEvent;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-public class AlarmListItemOnGestureListener extends SimpleOnGestureListener {
+public class AlarmListItemOnGestureListener extends SimpleOnGestureListener
+		implements AnimationListener {
 
 	private static final int SWIPE_MIN_DISTANCE = 150;
 	private static final int SWIPE_MAX_OFF_PATH = 100;
 	private static final int SWIPE_THRESHOLD_VELOCITY = 100;
 	private ListView lv;
+	private int lastDeletedPosition;
 
 	public AlarmListItemOnGestureListener(ListView lv) {
 		this.lv = lv;
@@ -30,12 +35,13 @@ public class AlarmListItemOnGestureListener extends SimpleOnGestureListener {
 			return false;
 		}
 
-		int pos = lv.pointToPosition((int) e1.getX(), (int) e1.getY());
-		if (AdapterView.INVALID_POSITION == pos) {
+		lastDeletedPosition = lv.pointToPosition((int) e1.getX(),
+				(int) e1.getY());
+		if (AdapterView.INVALID_POSITION == lastDeletedPosition) {
 			return false;
 		}
 
-		AlarmListItem item = (AlarmListItem) lv.getChildAt(pos);
+		AlarmListItem item = (AlarmListItem) lv.getChildAt(lastDeletedPosition);
 
 		if (null == item) {
 			return false;
@@ -47,12 +53,16 @@ public class AlarmListItemOnGestureListener extends SimpleOnGestureListener {
 		if (Math.abs(dY) < SWIPE_MAX_OFF_PATH
 				&& Math.abs(velocityX) >= SWIPE_THRESHOLD_VELOCITY
 				&& Math.abs(dX) >= SWIPE_MIN_DISTANCE) {
+			Animation anim;
 			if (dX > 0) {
-				item.animateOut(true);
+				anim = AnimationUtils.makeOutAnimation(item.getContext(), true);
 			} else {
-				item.animateOut(false);
+				anim = AnimationUtils
+						.makeOutAnimation(item.getContext(), false);
 			}
-			((AlarmListAdapter) lv.getAdapter()).removeAlarm(pos);
+			anim.setAnimationListener(this);
+			item.startAnimation(anim);
+
 			return true;
 		}
 		return false;
@@ -76,6 +86,23 @@ public class AlarmListItemOnGestureListener extends SimpleOnGestureListener {
 	@Override
 	public boolean onSingleTapUp(MotionEvent e) {
 		return false;
+	}
+
+	@Override
+	public void onAnimationEnd(Animation arg0) {
+		if (AdapterView.INVALID_POSITION == lastDeletedPosition) {
+			return;
+		}
+		((AlarmListAdapter) lv.getAdapter()).removeAlarm(lastDeletedPosition);
+		lastDeletedPosition = AdapterView.INVALID_POSITION;
+	}
+
+	@Override
+	public void onAnimationRepeat(Animation arg0) {
+	}
+
+	@Override
+	public void onAnimationStart(Animation arg0) {
 	}
 
 }
