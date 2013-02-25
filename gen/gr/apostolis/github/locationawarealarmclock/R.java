8f297d058755f32b7f00d547dfa2ae9a4469a720
Apostolis Github <a.github@gmail.com>
2013-02-25 11:24:53 +0200

    Fixed layout issue in add alarm activity
     - The add new alarm activity allows for scrolling on smaller
        screens or in landscape mode

--- gen/gr/apostolis/github/locationawarealarmclock/R.java
+++ gen/gr/apostolis/github/locationawarealarmclock/R.java
@@ -25,27 +25,29 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int add_alarm_lbl=0x7f080000;
-        public static final int add_button=0x7f08000c;
-        public static final int alarm_is_active=0x7f080012;
-        public static final int alarm_repeat=0x7f080011;
-        public static final int alarm_trigger_time=0x7f080010;
-        public static final int cancel_button=0x7f08000b;
-        public static final int dummy_button=0x7f08000f;
-        public static final int fri_tgl=0x7f080009;
-        public static final int fullscreen_content=0x7f08000d;
-        public static final int fullscreen_content_controls=0x7f08000e;
-        public static final int menu_add_alarm=0x7f080014;
-        public static final int menu_settings=0x7f080013;
-        public static final int mon_tgl=0x7f080005;
-        public static final int repeat_chk=0x7f080003;
-        public static final int sat_tgl=0x7f08000a;
-        public static final int sun_tgl=0x7f080004;
-        public static final int thu_tgl=0x7f080008;
-        public static final int timePicker=0x7f080002;
-        public static final int time_lbl=0x7f080001;
-        public static final int tue_tgl=0x7f080006;
-        public static final int wed_tgl=0x7f080007;
+        public static final int add_button=0x7f08000e;
+        public static final int alarm_is_active=0x7f080014;
+        public static final int alarm_repeat=0x7f080013;
+        public static final int alarm_trigger_time=0x7f080012;
+        public static final int cancel_button=0x7f08000d;
+        public static final int dummy_button=0x7f080011;
+        public static final int fri_tgl=0x7f08000b;
+        public static final int fullscreen_content=0x7f08000f;
+        public static final int fullscreen_content_controls=0x7f080010;
+        public static final int main_controls=0x7f080000;
+        public static final int menu_add_alarm=0x7f080016;
+        public static final int menu_settings=0x7f080015;
+        public static final int mon_tgl=0x7f080007;
+        public static final int repeat_buttons=0x7f080005;
+        public static final int repeat_chk=0x7f080004;
+        public static final int sat_tgl=0x7f08000c;
+        public static final int save_cancel_buttons=0x7f080001;
+        public static final int sun_tgl=0x7f080006;
+        public static final int thu_tgl=0x7f08000a;
+        public static final int timePicker=0x7f080003;
+        public static final int time_lbl=0x7f080002;
+        public static final int tue_tgl=0x7f080008;
+        public static final int wed_tgl=0x7f080009;
     }
     public static final class layout {
         public static final int activity_add_new_alarm=0x7f030000;
@@ -60,19 +62,18 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int activity_alarm_settings=0x7f070002;
     }
     public static final class string {
-        public static final int _add=0x7f05000c;
-        public static final int _cancel=0x7f05000d;
-        public static final int _fri=0x7f050013;
-        public static final int _mon=0x7f05000f;
-        public static final int _repeat=0x7f05000b;
-        public static final int _sat=0x7f050014;
-        public static final int _sun=0x7f05000e;
-        public static final int _thu=0x7f050012;
-        public static final int _time=0x7f05000a;
-        public static final int _tue=0x7f050010;
-        public static final int _wed=0x7f050011;
+        public static final int _add=0x7f05000b;
+        public static final int _cancel=0x7f05000c;
+        public static final int _fri=0x7f050012;
+        public static final int _mon=0x7f05000e;
+        public static final int _repeat=0x7f05000a;
+        public static final int _sat=0x7f050013;
+        public static final int _sun=0x7f05000d;
+        public static final int _thu=0x7f050011;
+        public static final int _time=0x7f050009;
+        public static final int _tue=0x7f05000f;
+        public static final int _wed=0x7f050010;
         public static final int app_name=0x7f050000;
-        public static final int create_alarm=0x7f050009;
         public static final int dummy_button=0x7f050006;
         public static final int dummy_content=0x7f050007;
         public static final int hello_world=0x7f050002;
