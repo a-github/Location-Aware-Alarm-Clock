2377a3bdc60008712c17acaa8493d1cc96d04226
Apostolis Github <a.github@gmail.com>
2013-02-26 19:03:25 +0200

    Moved Gesture Listener functionality to AlarmList
     - In order to be able to start activities on various events, the
        AlarmListItemOnGestureListener functionality had to be moved to
        the UI thread.

--- src/gr/apostolis/github/locationawarealarmclock/activities/AlarmList.java
+++ src/gr/apostolis/github/locationawarealarmclock/activities/AlarmList.java
@@ -3,7 +3,7 @@ package gr.apostolis.github.locationawarealarmclock.activities;
 import gr.apostolis.github.locationawarealarmclock.R;
 import gr.apostolis.github.locationawarealarmclock.adapters.AlarmListAdapter;
 import gr.apostolis.github.locationawarealarmclock.alarms.Alarm;
-import gr.apostolis.github.locationawarealarmclock.layouts.AlarmListItemOnGestureListener;
+import gr.apostolis.github.locationawarealarmclock.layouts.AlarmListItem;
 import gr.apostolis.github.locationawarealarmclock.layouts.UndoBarController;
 import gr.apostolis.github.locationawarealarmclock.layouts.UndoBarController.UndoListener;
 import android.app.ListActivity;
@@ -15,22 +15,33 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Animation.AnimationListener;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
-public class AlarmList extends ListActivity implements UndoListener {
+public class AlarmList extends ListActivity implements UndoListener,
+		OnGestureListener, AnimationListener {
 
 	private ListView alarmList;
 	private UndoBarController undoBarController;
+	private int lastDeletedPosition;
+	private static final int SWIPE_MIN_DISTANCE = 150;
+	private static final int SWIPE_MAX_OFF_PATH = 100;
+	private static final int SWIPE_THRESHOLD_VELOCITY = 100;
+	private static final int REQUEST_ALARM_NEW = 1;
+	private static final int REQUEST_ALARM_UPDATE = 2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_alarm_list);
-		alarmList = (ListView) findViewById(android.R.id.list);
+		alarmList = getListView();
 		alarmList.setAdapter(new AlarmListAdapter(getApplication(), this));
 		final GestureDetector gestureDetector = new GestureDetector(
-				alarmList.getContext(), new AlarmListItemOnGestureListener(
-						alarmList));
+				alarmList.getContext(), this);
 		View.OnTouchListener gestureListener = new View.OnTouchListener() {
 			public boolean onTouch(View v, MotionEvent event) {
 				boolean handled = gestureDetector.onTouchEvent(event);
@@ -48,6 +59,123 @@ public class AlarmList extends ListActivity implements UndoListener {
 	}
 
 	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		undoBarController.onSaveInstanceState(outState);
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle state) {
+		super.onRestoreInstanceState(state);
+		undoBarController.onRestoreInstanceState(state);
+	}
+
+	@Override
+	public void onAnimationEnd(Animation arg0) {
+		if (AdapterView.INVALID_POSITION == lastDeletedPosition) {
+			return;
+		}
+		((AlarmListAdapter) alarmList.getAdapter())
+				.removeAlarm(lastDeletedPosition);
+		lastDeletedPosition = AdapterView.INVALID_POSITION;
+	}
+
+	@Override
+	public void onAnimationRepeat(Animation animation) {
+	}
+
+	@Override
+	public void onAnimationStart(Animation animation) {
+	}
+
+	@Override
+	public boolean onDown(MotionEvent e) {
+		return false;
+	}
+
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		if (e1 == null || e2 == null) {
+			return false;
+		}
+
+		lastDeletedPosition = alarmList.pointToPosition((int) e1.getX(),
+				(int) e1.getY());
+		if (AdapterView.INVALID_POSITION == lastDeletedPosition) {
+			return false;
+		}
+
+		AlarmListItem item = (AlarmListItem) alarmList
+				.getChildAt(lastDeletedPosition);
+
+		if (null == item) {
+			return false;
+		}
+
+		float dX = e2.getX() - e1.getX();
+		float dY = e1.getY() - e2.getY();
+
+		if (Math.abs(dY) < SWIPE_MAX_OFF_PATH
+				&& Math.abs(velocityX) >= SWIPE_THRESHOLD_VELOCITY
+				&& Math.abs(dX) >= SWIPE_MIN_DISTANCE) {
+			Animation anim;
+			if (dX > 0) {
+				anim = AnimationUtils.makeOutAnimation(item.getContext(), true);
+			} else {
+				anim = AnimationUtils
+						.makeOutAnimation(item.getContext(), false);
+			}
+			anim.setAnimationListener(this);
+			item.startAnimation(anim);
+
+			return true;
+		}
+		return false;
+
+	}
+
+	@Override
+	public void onLongPress(MotionEvent e) {
+		int position = alarmList
+				.pointToPosition((int) e.getX(), (int) e.getY());
+		if (AdapterView.INVALID_POSITION == position) {
+			return;
+		}
+
+		AlarmListItem item = (AlarmListItem) alarmList.getChildAt(position);
+
+		if (null == item) {
+			return;
+		}
+
+		Intent intent = new Intent(alarmList.getContext(), AddNewAlarm.class);
+		intent.putExtra("ALARM", item.getAlarm());
+		startActivityForResult(intent, REQUEST_ALARM_UPDATE);
+
+	}
+
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+			float distanceY) {
+		return false;
+	}
+
+	@Override
+	public void onShowPress(MotionEvent e) {
+	}
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+		return false;
+	}
+
+	@Override
+	public void onUndo(Parcelable token) {
+		((AlarmListAdapter) alarmList.getAdapter()).undoDeleteAlarm();
+	}
+
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_alarm_list, menu);
 		return true;
@@ -58,7 +186,7 @@ public class AlarmList extends ListActivity implements UndoListener {
 		switch (item.getItemId()) {
 		case R.id.menu_add_alarm:
 			Intent intent = new Intent(this, AddNewAlarm.class);
-			startActivityForResult(intent, 1);
+			startActivityForResult(intent, REQUEST_ALARM_NEW);
 			break;
 		case R.id.menu_settings:
 			return false;
@@ -73,31 +201,17 @@ public class AlarmList extends ListActivity implements UndoListener {
 		if (RESULT_OK != resultCode) {
 			return;
 		}
-		if (1 == requestCode) {
-			Bundle extras = data.getExtras();
-			String time = extras.getString("time");
-			boolean[] repeatOn = extras.getBooleanArray("repeatOn");
-			Alarm alarm = new Alarm();
-			alarm.setTime(time);
-			alarm.setRepeatOn(repeatOn);
+
+		Bundle extras = data.getExtras();
+		Alarm alarm = (Alarm) extras.getSerializable("ALARM");
+		switch (requestCode) {
+		case REQUEST_ALARM_NEW:
 			((AlarmListAdapter) alarmList.getAdapter()).addAlarm(alarm);
+			break;
+		case REQUEST_ALARM_UPDATE:
+			((AlarmListAdapter) alarmList.getAdapter()).saveAlarm(alarm);
+			break;
+		default:
 		}
-	}
-
-	@Override
-	public void onUndo(Parcelable token) {
-		((AlarmListAdapter) alarmList.getAdapter()).undoDeleteAlarm();
-	}
-
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		undoBarController.onSaveInstanceState(outState);
-	}
-
-	@Override
-	protected void onRestoreInstanceState(Bundle state) {
-		super.onRestoreInstanceState(state);
-		undoBarController.onRestoreInstanceState(state);
 	}
 }
